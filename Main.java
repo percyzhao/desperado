9041463503f4a72040ba9437610e9f4eb13c9e7c
percyzhao <99761957+percyzhao@users.noreply.github.com>
2023-01-10 17:54:36 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -8,31 +8,37 @@ public class Main {
 	static Container c;
 	static CardLayout cards;
 	static KeyEvent e;
+
+
+
 	public static void main(String[] a) throws InterruptedException {
-		  
-		
+
+
 		JFrame f = new JFrame();
 		c = f.getContentPane();
 		cards = new CardLayout();
 		c.setLayout(cards);
-		
-		
-		
-		//Panel 1, MyPanel, is the home screen.
-		MyPanel panel = new MyPanel();
-		
-		//Panel 2, MyMain(), is currently a knight testing screen.
-		MyMain panel2 = new MyMain();
 
-		
-		c.add(panel);
-		c.add(panel2);
-		
-		f.setSize(700, 600);
+
+
+		//Panel 1, MyPanel, is the home screen.
+		//MyPanel panel = new MyPanel();
+
+		//Panel 2, MyMain(), is currently a knight testing screen.
+		//MyMain panel2 = new MyMain();
+
+		IntroFloor intro = new IntroFloor();
+
+		//c.add(panel);
+		//c.add(panel2);
+		c.add(intro);
+
+		f.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		f.setVisible(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
-		
+
+
+
+
 	}
 }
--- Player.java
+++ Player.java
@@ -1,23 +1,32 @@
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Player extends Entity{
-	
+
 	/*
 	 * 
 	 */
 	private static final String KNIGHT_IDLE_PATH = "knight_idle_spritesheet.png";
 	private static final String KNIGHT_RUN_PATH = "knight_run_spritesheet.png";
-	
-	
-	
+	private BufferedImage buffSword;
+	private Image sword;
+
+
+
 	public Player(int x, int y, int xVelocity, int yVelocity) {
-		super(x,y, xVelocity, yVelocity, KNIGHT_IDLE_PATH, KNIGHT_RUN_PATH, 100, 100);
-		
+
+		super(x, y, xVelocity, yVelocity, KNIGHT_IDLE_PATH, KNIGHT_RUN_PATH, 100, 100, 4);
+
+
 	}
-	
 
-	
 
-	
+
 
 }
