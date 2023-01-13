32f268471292a3645296b59e791d154eb2f57cad
haodongwang3 <99744252+haodongwang3@users.noreply.github.com>
2023-01-13 11:38:30 -0500

    Add files via upload

--- FloorOne.java
+++ FloorOne.java
@@ -1,6 +1,14 @@
 
+
+
+@SuppressWarnings("serial")
 public class FloorOne extends Level{
-	public FloorOne(int playerX, int playerY, int vx, int vy, String mapPath, String gridPath, int gridColumns, int gridRows) throws InterruptedException {
-        super(playerX, playerY, vx, vy, mapPath, gridPath, gridColumns, gridRows, 0, 3000, 0, 2100, 5);
-    }
+	
+		public FloorOne(int playerX, int playerY, int vx, int vy, String mapPath, String gridPath, int gridColumns, int gridRows) throws InterruptedException {
+			super(playerX, playerY, vx, vy, mapPath, gridPath, gridColumns, gridRows, 0, 3000, 0, 2100, 1904, 1088);
+			
+		}
+		
+		
+		
 }
